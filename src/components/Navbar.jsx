ea4dd5a243cce97b45a3aa10b95f66c4c3c2b823
Talal Ahmed <talalsidd31@gmail.com>
2024-02-03 03:35:28 +0500

    making navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
