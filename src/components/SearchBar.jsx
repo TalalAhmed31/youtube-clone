469edd0dc14fc2f00e7d36f9389dfe43b19e121b
Talal Ahmed <talalsidd31@gmail.com>
2024-02-04 15:49:36 +0500

    searchbar addition

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+// import {SearchIcon} from '@mui/icons-material';
+
+const SearchBar = () => {
+  return <Paper 
+  component="form" onSubmit={() => {}}
+  sx={{
+    borderRadius:20,
+    border: '1px solid #e3e3e3',
+    pl: 2,
+    boxShadow: 'none',
+    mr: {sm:5}
+  }}
+  >
+test
+  </Paper>;
+};
+
+export default SearchBar;
