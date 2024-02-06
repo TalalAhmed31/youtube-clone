d9e9ba962ef8467f230ea05c18957b404b4c809a
Talal Ahmed <talalsidd31@gmail.com>
2024-02-06 19:38:26 +0500

    video api fetch complete

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelCard = () => {
+  return (
+    <div>
+      channel card
+    </div>
+  )
+}
+
+export default ChannelCard
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
 const Videos = ({ videos }) => {
   return (
-    <div>
-      videos
-    </div>
-  )
-}
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+        {item.id.videoId && <VideoCard video={item} />}
+        {/* {item.id.channelId && <ChannelCard ChannelDetail={item} />} */}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
