6fd7ba5679c7ed6198c18bfbff2461e45efc10cf
Talal Ahmed <talalsidd31@gmail.com>
2024-02-13 01:52:16 +0500

    homepage finalized

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,5 +1,7 @@
 import { Box, CardContent, CardMedia, Typography} from '@mui/material';
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {Videos, ChannelCard} from "./";
 const ChannelDetail = () => {
   return (
     <div>
